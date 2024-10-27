b20554abe9b1f92ccb64acbf7f873e35eca4dc51
Guillermo Trejo <maynardtrejo@yahoo.com>
2024-10-26 21:44:30 -0500

    rounded the corners on the project section

--- Components/Projects.tsx
+++ Components/Projects.tsx
@@ -11,23 +11,23 @@ export const Projects = () => {
       </h1>
       <ul className="flex flex-wrap justify-center gap-4 mb-10 p-6">
         <li className="mr-4">
-          <div className="mb-10 rounded-lg shadow-lg shadow-green-800">
-            <Image src="/lingo.png" className="w-full" alt="Lingo" width={500} height={500} />
+          <div className="mb-10 rounded-lg shadow-xl shadow-green-800">
+            <Image src="/lingo.png" className="w-full rounded-xl" alt="Lingo" width={500} height={500} />
           </div>
         </li>
         <li className="">
-          <div className="mb-10 rounded-lg shadow-lg shadow-green-800">
-            <Image src="/internship.png" className="w-full" alt="Internship" width={500} height={500} />
+          <div className="mb-10 rounded-lg shadow-xl shadow-green-800">
+            <Image src="/internship.png" className="w-full rounded-lg" alt="Internship" width={500} height={500} />
           </div>
         </li>
         <li className="mr-4">
-          <div className="mb-10 rounded-lg shadow-lg shadow-green-800">
-            <Image src="/creations.png" className="w-full" alt="Creations" width={500} height={500} />
+          <div className="mb-10 rounded-lg shadow-xl shadow-green-800">
+            <Image src="/creations.png" className="w-full rounded-lg" alt="Creations" width={500} height={500} />
           </div>
         </li>
         <li className="">
-          <div className="mb-10 rounded-lg shadow-lg shadow-green-800">
-            <Image src="/netflix.png" className="w-full" alt="Netflix" width={500} height={500} />
+          <div className="mb-10 rounded-lg shadow-xl shadow-green-800">
+            <Image src="/netflix.png" className="w-full rounded-lg" alt="Netflix" width={500} height={500} />
           </div>
         </li>
       </ul>
