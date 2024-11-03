652e18a629164ca18b4d47ff38fea0958f77e905
Guillermo Trejo <maynardtrejo@yahoo.com>
2024-11-02 20:06:25 -0500

    fixed bg border on projects

--- Components/Projects.tsx
+++ Components/Projects.tsx
@@ -10,20 +10,20 @@ export const Projects = () => {
           <h1 className="text-center text-3xl mb-6">
             Here are some of my <span className="text-green-500">projects</span>
           </h1>
-          <ul className="flex flex-wrap justify-center gap-4 mb-10 p-6">
-            <li className="relative lg:mr-4 lg:mb-4">
-              <div className="mb-10 rounded-lg shadow-xl shadow-green-800 h-full">
+          <ul className="flex flex-wrap justify-center gap-4 mb-10 p-6 rounded-3xl">
+            <li className="relative lg:mb-4 top-0 left-0 w-[1000px] hover:bg-[#1c1d25] opacity-1 hover:opacity-55 transition duration-300 ease-in-out rounded-3xl">
+              <div className="mb-10 rounded-3xl shadow-xl shadow-green-800 h-full">
                 <Image
                   src="/lingo.png"
-                  className="w-full rounded-xl"
+                  className="w-full h-full rounded-3xl"
                   alt="Lingo"
                   width={500}
                   height={500}
                 />
               </div>
-              <div className="absolute top-0 left-0 w-full h-full hover:bg-[#1c1d25] opacity-0 hover:opacity-55 transition duration-300 ease-in-out"></div>
+              <div className="absolute top-0 left-0 w-full h-full rounded-3xl hover:bg-[#1c1d25] opacity-0 hover:opacity-55 transition duration-300 ease-in-out"></div>
               <div className="text-md absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 hover:opacity-100 transition duration-300 ease-in-out">
-                <h3 className="font-extrabold text-[48px] text-green-500 hover:visible z-[9999]">
+                <h3 className="font-extrabold text-[120px] fade-in text-green-500 hover:visible z-[9999]">
                   Lingo
                 </h3>
                 <h4 className="text-black-500 font-bold text-lg mb-1">
@@ -45,19 +45,19 @@ export const Projects = () => {
                 </div>
               </div>
             </li>
-            <li className="relative lg:mr-4 lg:mb-4">
-              <div className="mb-10 rounded-lg shadow-xl shadow-green-800 h-full">
+            <li className="relative lg:mb-4 top-0 left-0 w-[1000px] hover:bg-[#1c1d25] opacity-1 hover:opacity-55 transition duration-300 ease-in-out rounded-3xl">
+              <div className="mb-10 rounded-3xl shadow-xl shadow-green-800 h-full">
                 <Image
                   src="/creations.png"
-                  className="w-full rounded-xl"
+                  className="w-full h-full rounded-3xl"
                   alt="Lingo"
                   width={500}
                   height={500}
                 />
               </div>
-              <div className="absolute top-0 left-0 w-full h-full hover:bg-[#1c1d25] opacity-0 hover:opacity-55 transition duration-300 ease-in-out"></div>
+              <div className="absolute top-0 left-0 w-full h-full rounded-3xl hover:bg-[#1c1d25] opacity-0 hover:opacity-55 transition duration-300 ease-in-out"></div>
               <div className="text-md absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 hover:opacity-100 transition duration-300 ease-in-out">
-                <h3 className="font-extrabold text-[48px] text-green-500 hover:visible z-[9999]">
+                <h3 className="font-extrabold text-[120px] fade-in text-green-500 hover:visible z-[9999]">
                   Lingo
                 </h3>
                 <h4 className="text-black-500 font-bold text-lg mb-1">
@@ -79,19 +79,19 @@ export const Projects = () => {
                 </div>
               </div>
             </li>
-            <li className="relative lg:mr-4 lg:mb-4">
-              <div className="mb-10 rounded-lg shadow-xl shadow-green-800 h-full">
+            <li className="relative lg:mb-4 top-0 left-0 w-[1000px] hover:bg-[#1c1d25] opacity-1 hover:opacity-55 transition duration-300 ease-in-out rounded-3xl">
+              <div className="mb-10 rounded-3xl shadow-xl shadow-green-800 h-full">
                 <Image
                   src="/internship.png"
-                  className="w-full rounded-xl"
+                  className="w-full h-full rounded-3xl"
                   alt="Lingo"
                   width={500}
                   height={500}
                 />
               </div>
-              <div className="absolute top-0 left-0 w-full h-full hover:bg-[#1c1d25] opacity-0 hover:opacity-55 transition duration-300 ease-in-out"></div>
+              <div className="absolute top-0 left-0 w-full h-full rounded-3xl hover:bg-[#1c1d25] opacity-0 hover:opacity-55 transition duration-300 ease-in-out"></div>
               <div className="text-md absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 hover:opacity-100 transition duration-300 ease-in-out">
-                <h3 className="font-extrabold text-[48px] text-green-500 hover:visible z-[9999]">
+                <h3 className="font-extrabold text-[120px] fade-in text-green-500 hover:visible z-[9999]">
                   Lingo
                 </h3>
                 <h4 className="text-black-500 font-bold text-lg mb-1">
@@ -113,19 +113,19 @@ export const Projects = () => {
                 </div>
               </div>
             </li>
-            <li className="relative lg:mr-4 lg:mb-4">
-              <div className="mb-10 rounded-lg shadow-xl shadow-green-800 h-full">
+            <li className="relative lg:mb-4 top-0 left-0 w-[1000px] hover:bg-[#1c1d25] opacity-1 hover:opacity-55 transition duration-300 ease-in-out rounded-3xl">
+              <div className="mb-10 rounded-3xl shadow-xl shadow-green-800 h-full">
                 <Image
                   src="/netflix.png"
-                  className="w-full h-full rounded-xl"
+                  className="w-full h-full rounded-3xl"
                   alt="Lingo"
                   width={500}
                   height={500}
                 />
               </div>
-              <div className="absolute top-0 left-0 w-full h-full hover:bg-[#1c1d25] opacity-0 hover:opacity-55 transition duration-300 ease-in-out"></div>
+              <div className="absolute top-0 left-0 w-full h-full rounded-3xl hover:bg-[#1c1d25] opacity-0 hover:opacity-55 transition duration-300 ease-in-out"></div>
               <div className="text-md absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 hover:opacity-100 transition duration-300 ease-in-out">
-                <h3 className="font-extrabold text-[48px] text-green-500 hover:visible z-[9999]">
+                <h3 className="font-extrabold text-[120px] fade-in text-green-500 hover:visible z-[9999]">
                   Lingo
                 </h3>
                 <h4 className="text-black-500 font-bold text-lg mb-1">
