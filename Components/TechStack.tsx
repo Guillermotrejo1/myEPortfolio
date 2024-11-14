6c679f66a8b15492b58f4c6f62228662afc666cb
Guillermo Trejo <maynardtrejo@yahoo.com>
2024-11-13 21:11:05 -0600

    added names on hover to the techstack

--- Components/TechStack.tsx
+++ Components/TechStack.tsx
@@ -2,28 +2,145 @@ import Image from "next/image";
 
 export const TechStack = () => {
   return (
-    <div className="flex flex-col justify-center items-center w-full mb-10 p-6">
+    <div className="container mb-10">
       <h1 className="flex justify-center text-3xl font-bold w-full mb-4 h-14">
         Tech Stack
       </h1>
       <div className="flex flex-wrap justify-center items-center gap-4">
-        <Image src="/html.png" alt="" width={100} height={100} />
-        <Image src="/css.png" alt="" width={100} height={100} />
-        <Image src="/React.png" alt="" width={100} height={100} />
-        <Image src="/firebase.webp" alt="" width={100} height={100} />
-        <Image src="/nextjs.png" alt="" width={100} height={100} />
-        <Image src="/redux.png" alt="" width={100} height={100} />
-        <Image src="/tailwind.png" alt="" width={100} height={100} />
-        <Image src="/nodejs.png" alt="" width={100} height={100} />
-        <Image src="/javascript.png" alt="" width={100} height={100} />
-        <Image src="/typescript.png" alt="" width={100} height={100} />
-        <Image
-          src="/drizzle.png"
-          className="rounded-lg"
-          alt=""
-          width={100}
-          height={100}
-        />
+        <div className="flex flex-wrap justify-center items-center gap-4">
+          <div className="group flex flex-col items-center h-48">
+            <Image
+              src="/html.png"
+              alt=""
+              className="hover:scale-110 transition-all duration-300 ease-out"
+              width={100}
+              height={100}
+            />
+            <span className="text-lg font-medium hidden group-hover:block transition-all duration-500 ease-in-out mt-4">
+              HTML
+            </span>
+          </div>
+          <div className="group flex flex-col items-center h-48">
+            <Image
+              src="/css.png"
+              alt=""
+              className="hover:scale-110 transition-all duration-300 ease-out"
+              width={100}
+              height={100}
+            />
+            <span className="text-lg font-medium hidden group-hover:block transition-all duration-500 ease-in-out mt-4">
+              CSS
+            </span>
+          </div>
+          <div className="group flex flex-col items-center h-48">
+            <Image
+              src="/React.png"
+              alt=""
+              className="hover:scale-110 transition-all duration-300 ease-out"
+              width={100}
+              height={100}
+            />
+            <span className="text-lg font-medium hidden group-hover:block transition-all duration-500 ease-in-out mt-4">
+              REACT
+            </span>
+          </div>
+          <div className="group flex flex-col items-center h-48">
+            <Image
+              src="/firebase.webp"
+              alt=""
+              className="hover:scale-110 transition-all duration-300 ease-out"
+              width={100}
+              height={100}
+            />
+            <span className="text-lg font-medium hidden group-hover:block transition-all duration-500 ease-in-out mt-4">
+              FIREBASE
+            </span>
+          </div>
+          <div className="group flex flex-col items-center h-48">
+            <Image
+              src="/nextjs.png"
+              alt=""
+              className="hover:scale-110 transition-all duration-300 ease-out"
+              width={100}
+              height={100}
+            />
+            <span className="text-lg font-medium hidden group-hover:block transition-all duration-500 ease-in-out mt-4">
+              NEXTJS
+            </span>
+          </div>
+          <div className="group flex flex-col items-center h-48">
+            <Image
+              src="/redux.png"
+              alt=""
+              className="hover:scale-110 transition-all duration-300 ease-out"
+              width={100}
+              height={100}
+            />
+            <span className="text-lg font-medium hidden group-hover:block transition-all duration-500 ease-in-out mt-4">
+              REDUX
+            </span>
+          </div>
+          <div className="group flex flex-col items-center h-48">
+            <Image
+              src="/tailwind.png"
+              alt=""
+              className="hover:scale-110 transition-all duration-300 ease-out"
+              width={100}
+              height={100}
+            />
+            <span className="text-lg font-medium hidden group-hover:block transition-all duration-500 ease-in-out mt-4">
+              TAILWIND
+            </span>
+          </div>
+          <div className="group flex flex-col items-center h-48">
+            <Image
+              src="/nodejs.png"
+              alt=""
+              className="hover:scale-110 transition-all duration-300 ease-out"
+              width={100}
+              height={100}
+            />
+            <span className="text-lg font-medium hidden group-hover:block transition-all duration-500 ease-in-out mt-4">
+              NODEJS
+            </span>
+          </div>
+          <div className="group flex flex-col items-center h-48">
+            <Image
+              src="/javascript.png"
+              alt=""
+              className="hover:scale-110 transition-all duration-300 ease-out rounded-xl"
+              width={100}
+              height={100}
+            />
+            <span className="text-lg font-medium hidden group-hover:block transition-all duration-500 ease-in-out mt-4">
+              JAVASCRIPT
+            </span>
+          </div>
+          <div className="group flex flex-col items-center h-48">
+            <Image
+              src="/typescript.png"
+              alt=""
+              className="hover:scale-110 transition-all duration-300 ease-out"
+              width={100}
+              height={100}
+            />
+            <span className="text-lg font-medium hidden group-hover:block transition-all duration-500 ease-in-out mt-4">
+              TYPESCRIPT
+            </span>
+          </div>
+          <div className="group flex flex-col items-center h-48">
+            <Image
+              src="/drizzle.png"
+              alt=""
+              className="hover:scale-110 transition-all duration-300 ease-out rounded-xl"
+              width={100}
+              height={100}
+            />
+            <span className="text-lg font-medium hidden group-hover:block transition-all duration-500 ease-in-out mt-4">
+              DRIZZLE
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
